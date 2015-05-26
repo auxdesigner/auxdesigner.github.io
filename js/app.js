e811e94d4144ac85cc4089b0b51d687b64dee680
Liu Liu <lllxjy2009@gmail.com>
2015-05-26 22:50:12 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var initParticleCount = Math.floor(document.body.clientWidth / 4),
+maxParticleCount = Math.ceil(1.2 * initParticleCount),
+particleCount = initParticleCount;
+/* config dom id (optional) + config particles params */
+particlesJS('particles-js', {
+  particles: {
+    color: "#4285F4",
+    color_random: ["#DB4437" /*red*/, "#FFEB3B" /*yellow*/ , "#0F9D58" /*green*/, "#4285F4" /*blue*/],
+    shape: 'circle',
+    opacity: {
+        opacity: .5
+        /*anim: {
+            enable: false,
+            speed: 0.8,
+            opacity_min: .1,
+            sync: false
+        }*/
+    },
+    size: 2,
+    size_random: true,
+    nb: initParticleCount,
+    line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: "#33b1f8",
+        opacity: .25,
+        width: 1
+    },
+    anim: {
+              enable: true,
+              speed: .8
+    }
+  },
+  interactivity: {
+    mode: false // "grab" or false
+  },
+  retina_detect: true
+});
