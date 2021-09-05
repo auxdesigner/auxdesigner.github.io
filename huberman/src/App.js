700e736f91848bc2068358ae4392264672a04c22
Liu Liu <liuliuweb@gmail.com>
2021-09-04 19:38:32 -0700

    picker

--- huberman/src/App.js
+++ huberman/src/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: "cat"};
-    this.change = this.change.bind(this);
-  }
-
-  change(event){
-      console.log(event.target.value);
-      this.setState({value: event.target.value})
+    this.state = {value: "07:00"};
+    this.changeTime = this.changeTime.bind(this);
   }
 
 
+  changeTime(event){
+    console.log(event.target.value);
+    this.setState({value: event.target.value})
+  }
 
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <select id="pet-select" onChange={this.change} value={this.state.value}>
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="hamster">Hamster</option>
-            <option value="parrot">Parrot</option>
-            <option value="spider">Spider</option>
-            <option value="goldfish">Goldfish</option>
-        </select>
         <h2>It is {this.state.value}.</h2>
+        <TextField
+          id="time"
+          label="Alarm clock"
+          type="time"
+          defaultValue={this.state.value}
+          inputProps={{
+            step: 900, // 5 min
+          }}
+          onChange={this.changeTime}
+        />
       </div>
     );
   }
