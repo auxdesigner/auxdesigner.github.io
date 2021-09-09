92491e5739b76819fd7c3738654838cb6bae58c4
Liu Liu <liuliuweb@gmail.com>
2021-09-09 12:31:49 -0700

    nested array

--- map-gen/src/App.js
+++ map-gen/src/App.js
@@ -3,29 +3,43 @@ import React from "react";
 import "./App.css";
 
 var blocks = [],
-  even = [],
-  odd = [],
-  cellTotal = 4,
-  rowTotal = 4,
+  rows = [],
+  cells = [],
+  //asset = ["house", "grass", "cow"],
+  cellTotal = 3,
+  rowTotal = 3,
   houseWidth = 3,
   houseHeight = 2;
 
-for (var i = 1; i <= cellTotal * rowTotal; i++) {
-  blocks.push(i);
-}
-
-blocks.forEach((i) => {
-  console.log(i);
-  if (i % 2 === 0) {
-    even.push(i);
-  } else {
-    odd.push(i);
+//generate all blocks
+for (var row = 0; row <= rowTotal; row++) {
+  rows.push([]);
+  for (var cell = 0; cell <= cellTotal; cell++) {
+    rows[row].push([row + "-" + cell]);
   }
-});
-
-function randomItem(i) {
-  return Math.floor(Math.random() * i.length);
 }
+console.log(rows);
+// for (var row = 1; row <= rowTotal; row++) {
+//   if (rowTotal - row + 1 >= houseHeight) {
+//     for (var cell = 1; cell <= cellTotal; cell++) {
+//       if (cellTotal - cell + 1 >= houseWidth) {
+//         blocks.push("+" + row + "-" + cell);
+//       } else {
+//         blocks.push(row + "-" + cell);
+//       }
+//     }
+//   } else {
+//     for (cell = 1; cell <= cellTotal; cell++) {
+//       blocks.push(row + "-" + cell);
+//     }
+//   }
+// }
+
+//find eligible blocks
+
+// function randomItem(i) {
+//   return Math.floor(Math.random() * i.length);
+// }
 
 class App extends React.Component {
   // constructor(props) {
@@ -35,24 +49,17 @@ class App extends React.Component {
   // }
 
   render() {
-    //var blocksDiv = blocks.map((item) => <div key={item}> {item} </div>);
-    var evenDiv = even.map((item) => (
-      <div className="even" key={item}>
-        {item}
-      </div>
-    ));
-    var oddDiv = odd.map((item) => (
-      <div className="odd" key={item}>
-        {item}
+    var rowsDiv = rows.map((row_item, row_index) => (
+      <div className={"row " + row_index} key={row_index}>
+        {row_item.map((cell_item, cell_index) => (
+          <div className={"cell " + cell_index} key={cell_index}>
+            {cell_item}
+          </div>
+        ))}
       </div>
     ));
 
-    return (
-      <div className="App">
-        {evenDiv}
-        {oddDiv}
-      </div>
-    );
+    return <div className="App">{rowsDiv}</div>;
   }
 }
 
