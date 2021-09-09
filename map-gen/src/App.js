1f2af26d60a36c3f442819b13b1972ea1df527aa
Liu Liu <liuliuweb@gmail.com>
2021-09-09 14:01:43 -0700

    random map

--- map-gen/src/App.js
+++ map-gen/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-//import logo from "./logo.svg";
 import "./App.css";
 
 var rows = [],
+  houseblocks = [],
+  sceneblocks = [],
   asset = ["grass", "cow", "dog"],
   cellTotal = 3, //4
   rowTotal = 3, //4
   houseWidth = 3,
   houseHeight = 2;
+
+function randomItem(i) {
+  return i[Math.floor(Math.random() * i.length)];
+}
 
 //generate all blocks
 for (var row = 0; row <= rowTotal; row++) {
@@ -16,18 +21,27 @@ for (var row = 0; row <= rowTotal; row++) {
     var eligibleRow = rowTotal + 1 - row >= houseHeight,
       eligibleCell = cellTotal + 1 - cell >= houseWidth;
     if (eligibleRow && eligibleCell) {
-      rows[row].push("YES");
+      houseblocks.push([row, cell]);
+
       //todo: add eligble cells to an array, and append image logic to set the adjacent arrays
-    } else {
-      rows[row].push(randomItem(asset));
     }
+    rows[row].push(row + "-" + cell);
   }
 }
-console.log(rows);
 
-function randomItem(i) {
-  return i[Math.floor(Math.random() * i.length)];
-}
+var selectedBlock = randomItem(houseblocks),
+  selectedRow = selectedBlock[0],
+  selectedCell = selectedBlock[1];
+//console.log(selectedCell);
+// selectedCell = "top-left";
+
+rows[selectedRow][selectedCell] = "TL";
+rows[selectedRow][selectedCell + 1] = "TM";
+rows[selectedRow][selectedCell + 2] = "TR";
+rows[selectedRow + 1][selectedCell] = "BL";
+rows[selectedRow + 1][selectedCell + 1] = "BM";
+rows[selectedRow + 1][selectedCell + 2] = "BR";
+console.log(rows);
 
 class App extends React.Component {
   // constructor(props) {
