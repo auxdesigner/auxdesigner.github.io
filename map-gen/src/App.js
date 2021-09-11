7f82a843193ff2e5e57ff29d32feea00646f44a4
Liu Liu <liuliuweb@gmail.com>
2021-09-11 13:37:23 -0700

    proof of concept

--- map-gen/src/App.js
+++ map-gen/src/App.js
@@ -3,12 +3,10 @@ import "./App.css";
 
 var rows = [],
   houseblocks = [],
-  sceneblocks = [],
-  asset = ["grass", "cow", "dog"],
-  cellTotal = 3, //4
-  rowTotal = 3, //4
+  cellTotal = 5, //6
+  rowTotal = 5, //6
   houseWidth = 3,
-  houseHeight = 2;
+  houseHeight = 4;
 
 function randomItem(i) {
   return i[Math.floor(Math.random() * i.length)];
@@ -18,29 +16,31 @@ function randomItem(i) {
 for (var row = 0; row <= rowTotal; row++) {
   rows.push([]);
   for (var cell = 0; cell <= cellTotal; cell++) {
+    //add eligible blocks
     var eligibleRow = rowTotal + 1 - row >= houseHeight,
       eligibleCell = cellTotal + 1 - cell >= houseWidth;
     if (eligibleRow && eligibleCell) {
       houseblocks.push([row, cell]);
-
-      //todo: add eligble cells to an array, and append image logic to set the adjacent arrays
     }
-    rows[row].push(row + "-" + cell);
+    //add all blocks
+    rows[row].push("land");
   }
 }
 
+//pick a block
 var selectedBlock = randomItem(houseblocks),
   selectedRow = selectedBlock[0],
   selectedCell = selectedBlock[1];
-//console.log(selectedCell);
-// selectedCell = "top-left";
 
-rows[selectedRow][selectedCell] = "TL";
-rows[selectedRow][selectedCell + 1] = "TM";
-rows[selectedRow][selectedCell + 2] = "TR";
-rows[selectedRow + 1][selectedCell] = "BL";
-rows[selectedRow + 1][selectedCell + 1] = "BM";
-rows[selectedRow + 1][selectedCell + 2] = "BR";
+//find the rest based on picked block
+rows[selectedRow][selectedCell] = selectedRow + "-" + selectedCell;
+
+for (var paintedRow = 0; paintedRow < houseHeight; paintedRow++) {
+  for (var paintedCell = 0; paintedCell < houseWidth; paintedCell++) {
+    rows[selectedRow + paintedRow][selectedCell + paintedCell] =
+      paintedRow + "-" + paintedCell;
+  }
+}
 console.log(rows);
 
 class App extends React.Component {
@@ -52,11 +52,9 @@ class App extends React.Component {
 
   render() {
     var blocks = rows.map((row_item, row_index) => (
-      <div className={"row " + row_index} key={row_index}>
+      <div className={"row"} key={row_index}>
         {row_item.map((cell_item, cell_index) => (
-          <div className={"cell " + cell_item} key={cell_index}>
-            {cell_item}
-          </div>
+          <div className={"cell img-" + cell_item} key={cell_index}></div>
         ))}
       </div>
     ));
