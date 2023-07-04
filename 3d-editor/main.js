251699b292cc956906816d65575bac45b33bceaf
Liu Liu <liuliuweb@gmail.com>
2023-07-04 13:32:43 -0700

    transform control

--- 3d-editor/main.js
+++ 3d-editor/main.js
@@ -1,97 +1,89 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { TransformControls } from "three/examples/jsm/controls/TransformControls.js";
-import { ViewHelper } from "three/examples/jsm/helpers/ViewHelper.js";
+import { InteractionManager } from "three.interactive";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { TransformControls } from "three/addons/controls/TransformControls.js";
 
-let cube, renderer, scene, camera, controls, helper, clock;
+const container = document.createElement("div");
+container.setAttribute("id", "container");
+document.body.appendChild(container);
 
-init();
-animate();
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
 
-function init() {
-  // renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.autoClear = false;
-  document.body.appendChild(renderer.domElement);
+// scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xfefefe);
 
-  // scene
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xf0f0f0);
+// camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  10000
+);
+camera.position.set(10, 10, 10);
 
-  // camera
-  camera = new THREE.PerspectiveCamera(
-    40,
-    window.innerWidth / window.innerHeight,
-    1,
-    10000
-  );
-  camera.position.set(10, 10, 10);
+// light
+const light = new THREE.DirectionalLight(0xf000ff, 1);
+light.position.set(20, 20, 0);
+scene.add(light);
 
-  // controls
-  controls = new OrbitControls(camera, renderer.domElement);
+// axes
+scene.add(new THREE.AxesHelper(10));
 
-  // clock
-  clock = new THREE.Clock();
+// object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x4285f4,
+});
+const cube = new THREE.Mesh(geometry, material);
 
-  // ambient
-  scene.add(new THREE.AmbientLight(0x222222));
+// interaction
+const interactionManager = new InteractionManager(
+  renderer,
+  camera,
+  renderer.domElement
+);
+cube.addEventListener("mouseover", (event) => {
+  event.target.material.color.set(0xff0000);
+  document.body.style.cursor = "pointer";
+});
+cube.addEventListener("mouseout", (event) => {
+  event.target.material.color.set(0x4285f4);
+  document.body.style.cursor = "default";
+});
+cube.addEventListener("mousedown", (event) => {
+  event.target.scale.set(1.1, 1.1, 1.1);
+});
+cube.addEventListener("click", (event) => {
+  event.target.scale.set(1.0, 1.0, 1.0);
+});
 
-  // light
-  const light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(20, 20, 0);
-  scene.add(light);
+scene.add(cube);
+interactionManager.add(cube);
 
-  // axes
-  scene.add(new THREE.AxesHelper(20));
+// orbital control
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
-  // cube
-  const newCube = ([x, y, z]) => {
-    // geometry
-    const geometry = new THREE.BoxGeometry();
-    // material
-    const material = new THREE.MeshNormalMaterial();
-    // make cube
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.x = x;
-    cube.position.y = y;
-    cube.position.z = z;
-    return cube;
-  };
-  const cube1 = newCube([1, 1, 1]);
-  const cube2 = newCube([1, 1, 3]);
-  scene.add(cube1);
-  scene.add(cube2);
+// transform control
+const transformControls = new TransformControls(camera, renderer.domElement);
 
-  // helper
-  helper = new ViewHelper(camera, renderer.domElement);
-  helper.controls = controls;
-  helper.controls.center = controls.target;
+transformControls.addEventListener("dragging-changed", function (event) {
+  controls.enabled = !event.value;
+});
 
-  const div = document.createElement("div");
-  div.id = "viewHelper";
-  div.style.position = "absolute";
-  div.style.right = 0;
-  div.style.bottom = 0;
-  div.style.height = "128px";
-  div.style.width = "128px";
+scene.add(transformControls);
+transformControls.attach(cube);
 
-  document.body.appendChild(div);
-
-  div.addEventListener("pointerup", (event) => helper.handleClick(event));
-}
-
-function animate() {
+const animate = (time) => {
   requestAnimationFrame(animate);
-
-  const delta = clock.getDelta();
-
-  if (helper.animating) helper.update(delta);
-
-  renderer.clear();
-
+  interactionManager.update();
+  controls.update();
   renderer.render(scene, camera);
+};
 
-  helper.render(renderer);
-}
+animate();
