d05ec053e87aa6286ad203b0ccc75a745182c152
Liu Liu <liuliuweb@gmail.com>
2023-07-05 14:16:20 -0700

    remove

--- 3d-editor/main.js
+++ 3d-editor/main.js
@@ -1,12 +1,20 @@
 import * as THREE from "three";
 import { InteractionManager } from "three.interactive";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { TransformControls } from "three/addons/controls/TransformControls.js";
 
-// create dom
-const container = document.createElement("div");
-container.setAttribute("id", "container");
-document.body.appendChild(container);
+// sidebar
+const buttons = document.querySelectorAll("#sidebar button");
+let selectedTool = "cube";
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove("selected");
+    });
+
+    button.classList.add("selected");
+    selectedTool = button.id;
+  });
+});
 
 // renderer
 const renderer = new THREE.WebGLRenderer();
@@ -55,14 +63,71 @@ function initialize(size) {
         mesh.material.emissive.setHex(0);
       });
       mesh.addEventListener("click", (event) => {
-        console.log(event.target.position);
+        const position = event.target.position;
         event.stopPropagation();
+
+        // if cube is selected in toolbar
+        if (selectedTool === "cube") {
+          newCube([position.x, position.y, position.z], 0xffff00);
+        }
+
+        // if tree is selected in toolbar
+        if (selectedTool === "tree") {
+          newTree([position.x, position.y, position.z], 0x00ff00);
+        }
       });
       interactionManager.add(mesh);
     }
   }
 }
 initialize(8);
+
+// create cube
+function newCube([x, y, z], color) {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshLambertMaterial({ color });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = x;
+  mesh.position.y = y + 1;
+  mesh.position.z = z;
+  scene.add(mesh);
+
+  mesh.addEventListener("mouseover", (event) => {
+    mesh.material.emissive.setHex(0x555555);
+  });
+  mesh.addEventListener("mouseout", (event) => {
+    mesh.material.emissive.setHex(0);
+  });
+  mesh.addEventListener("click", (event) => {
+    const position = event.target.position;
+    event.stopPropagation();
+
+    // if eraser is selected in toolbar
+    if (selectedTool === "eraser") {
+      scene.remove(mesh);
+    }
+  });
+  interactionManager.add(mesh);
+}
+
+// create tree
+function newTree([x, y, z], color) {
+  const geometry = new THREE.BoxGeometry(1, 2, 1);
+  const material = new THREE.MeshLambertMaterial({ color });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = x;
+  mesh.position.y = y + 1;
+  mesh.position.z = z;
+  scene.add(mesh);
+
+  mesh.addEventListener("mouseover", (event) => {
+    mesh.material.emissive.setHex(0x555555);
+  });
+  mesh.addEventListener("mouseout", (event) => {
+    mesh.material.emissive.setHex(0);
+  });
+  interactionManager.add(mesh);
+}
 
 // orbital control
 const orbitalControls = new OrbitControls(camera, renderer.domElement);
@@ -76,5 +141,3 @@ const animate = (time) => {
 };
 
 animate();
-
-//https://jsfiddle.net/7rdv6ox3/ helper widget
