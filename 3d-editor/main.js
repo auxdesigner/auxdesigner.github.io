0396bc2cbb2dbea9826036f0efde380da21b38be
Liu Liu <liuliuweb@gmail.com>
2023-07-06 14:12:01 -0700

    model

--- 3d-editor/main.js
+++ 3d-editor/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { InteractionManager } from "three.interactive";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // sidebar
 const buttons = document.querySelectorAll("#sidebar button");
@@ -36,9 +37,19 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(10, 10, 10);
 
 // light
-const light = new THREE.DirectionalLight(0xf000ff, 1);
-light.position.set(20, 20, 0);
-scene.add(light);
+const sun = new THREE.DirectionalLight(0xffffff, 1);
+sun.position.set(20, 20, 20);
+sun.castShadow = true;
+sun.shadow.camera.left = -10;
+sun.shadow.camera.right = 10;
+sun.shadow.camera.top = 0;
+sun.shadow.camera.bottom = -10;
+sun.shadow.mapSize.width = 1024;
+sun.shadow.mapSize.height = 1024;
+sun.shadow.camera.near = 0.5;
+sun.shadow.camera.far = 50;
+scene.add(sun);
+scene.add(new THREE.AmbientLight(0xffffff, 0.3));
 
 // interaction
 const interactionManager = new InteractionManager(
@@ -80,7 +91,7 @@ function initialize(size) {
     }
   }
 }
-initialize(8);
+initialize(2);
 
 // create cube
 function newCube([x, y, z], color) {
@@ -108,28 +119,37 @@ function newCube([x, y, z], color) {
 }
 
 // create tree
-function newTree([x, y, z], color) {
-  const geometry = new THREE.BoxGeometry(1, 2, 1);
-  const material = new THREE.MeshLambertMaterial({ color });
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.x = x;
-  mesh.position.y = y + 1;
-  mesh.position.z = z;
-  scene.add(mesh);
+// glb
+const loader = new GLTFLoader();
 
-  mesh.addEventListener("mouseover", (event) => {
-    mesh.material.emissive.setHex(0x555555);
-  });
-  mesh.addEventListener("mouseout", (event) => {
-    mesh.material.emissive.setHex(0);
-  });
-  mesh.addEventListener("click", (event) => {
-    event.stopPropagation();
-    if (selectedTool === "eraser") {
-      scene.remove(mesh);
+function newTree([x, y, z], color) {
+  loader.load(
+    "model/tree.glb",
+    function (glb) {
+      glb.scene.position.x = x;
+      glb.scene.position.y = y;
+      glb.scene.position.z = z;
+      scene.add(glb.scene);
+      glb.scene.addEventListener("mouseover", (event) => {
+        // console.log(glb.scene);
+      });
+
+      glb.scene.addEventListener("click", (event) => {
+        event.stopPropagation();
+        if (selectedTool === "eraser") {
+          scene.remove(glb.scene);
+          scene.children.forEach((child) => console.log(child));
+        }
+      });
+      interactionManager.add(glb.scene);
+    },
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    function (error) {
+      console.error(error);
     }
-  });
-  interactionManager.add(mesh);
+  );
 }
 
 // orbital control
