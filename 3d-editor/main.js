b6cafc0083ec8d93b18723c4e12052d875539626
Liu Liu <liuliuweb@gmail.com>
2023-07-07 09:42:48 -0700

    model

--- 3d-editor/main.js
+++ 3d-editor/main.js
@@ -1,11 +1,10 @@
 import * as THREE from "three";
-import { InteractionManager } from "three.interactive";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // sidebar
 const buttons = document.querySelectorAll("#sidebar button");
-let selectedTool = "cube";
+let selectedTool = "tree";
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttons.forEach((btn) => {
@@ -31,7 +30,7 @@ scene.background = new THREE.Color(0xfefefe);
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
-  1,
+  0.1,
   1000
 );
 camera.position.set(10, 10, 10);
@@ -57,7 +56,7 @@ function initialize(size) {
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
       const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+      const material = new THREE.MeshLambertMaterial({ color: 0x40bf15 });
       const cube = new THREE.Mesh(geometry, material);
       cube.position.set(x, 0, y);
       scene.add(cube);
@@ -69,17 +68,60 @@ initialize(8);
 
 // tree
 const trees = [];
-const loader = new GLTFLoader();
+const treeLoader = new GLTFLoader();
 function newTree([x, y, z]) {
-  loader.load(
+  treeLoader.load(
     "model/tree.glb",
     function (glb) {
       glb.scene.position.x = x;
-      glb.scene.position.y = y;
+      glb.scene.position.y = y + 0.5;
       glb.scene.position.z = z;
       scene.add(glb.scene);
       trees.push(glb.scene.children[0]);
-      console.log(trees);
+    },
+    function (xhr) {
+      // console.log(xhr);
+    },
+    function (error) {
+      console.error(error);
+    }
+  );
+}
+
+// mushroom
+const mushrooms = [];
+const mushroomLoader = new GLTFLoader();
+function newMushroom([x, y, z]) {
+  mushroomLoader.load(
+    "model/mushroom.glb",
+    function (glb) {
+      glb.scene.position.x = x;
+      glb.scene.position.y = y + 0.5;
+      glb.scene.position.z = z;
+      scene.add(glb.scene);
+      mushrooms.push(glb.scene.children[0]);
+    },
+    function (xhr) {
+      // console.log(xhr);
+    },
+    function (error) {
+      console.error(error);
+    }
+  );
+}
+
+// rock
+const rocks = [];
+const rockLoader = new GLTFLoader();
+function newRock([x, y, z]) {
+  rockLoader.load(
+    "model/rock.glb",
+    function (glb) {
+      glb.scene.position.x = x;
+      glb.scene.position.y = y + 0.5;
+      glb.scene.position.z = z;
+      scene.add(glb.scene);
+      rocks.push(glb.scene.children[0]);
     },
     function (xhr) {
       // console.log(xhr);
@@ -118,7 +160,6 @@ function onMouseHover(event) {
     }
     intersects[0].object.parent.children[0].material.emissive.setHex(0x555555);
     intersects[0].object.parent.children[1].material.emissive.setHex(0x555555);
-    // intersects[0].object.material.emissive.setHex(0x555555);
   }
   // reset highlight
   else {
@@ -144,7 +185,7 @@ function onMouseClick(event) {
   const raycaster = new THREE.Raycaster();
   raycaster.setFromCamera(mouse, camera);
   const intersects = raycaster.intersectObjects(scene.children);
-
+  // eraser tool
   if (selectedTool === "eraser") {
     // tree
     if (intersects.length > 0 && intersects[0].object.parent.name === "Tree") {
@@ -155,12 +196,32 @@ function onMouseClick(event) {
     if (intersects.length > 0 && cubes.includes(intersects[0].object)) {
       console.log("found ground");
     }
-  } else if (selectedTool === "tree") {
+  }
+  // tree tool
+  else if (selectedTool === "tree") {
     if (intersects.length > 0 && cubes.includes(intersects[0].object)) {
       let positionX = intersects[0].object.position.x;
       let positionY = intersects[0].object.position.y;
       let positionZ = intersects[0].object.position.z;
       newTree([positionX, positionY, positionZ]);
+    }
+  }
+  // mushroom tool
+  else if (selectedTool === "mushroom") {
+    if (intersects.length > 0 && cubes.includes(intersects[0].object)) {
+      let positionX = intersects[0].object.position.x;
+      let positionY = intersects[0].object.position.y;
+      let positionZ = intersects[0].object.position.z;
+      newMushroom([positionX, positionY, positionZ]);
+    }
+  }
+  // rock tool
+  else if (selectedTool === "rock") {
+    if (intersects.length > 0 && cubes.includes(intersects[0].object)) {
+      let positionX = intersects[0].object.position.x;
+      let positionY = intersects[0].object.position.y;
+      let positionZ = intersects[0].object.position.z;
+      newRock([positionX, positionY, positionZ]);
     }
   }
 }
